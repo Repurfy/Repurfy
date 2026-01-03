c83122e496b9f28c8fbb18f31a374b5f9cfe3aed
Rahul Kumar <kumar.coding.practice@gmail.com>
2026-01-03 23:28:48 +0530

    Create Page Content Updated

--- app/(dashboard)/create-content/page.tsx
+++ app/(dashboard)/create-content/page.tsx
@@ -9,7 +9,7 @@ export default function CreateContentPage() {
       <h1 className="text-2xl font-bold tracking-tight">Repurpose Content Across All Platforms</h1>
 
       <p className="text-text-secondary mt-1 mb-10 leading-relaxed">
-        Upload your content, and get AI-optimized posts for Facebook, LinkedIn, Instagram and more.
+        Upload your content, and get AI-optimized posts for Facebook, LinkedIn, Instagram & more.
       </p>
 
       {/* Main container check temp.tsx to get previouos code */}
