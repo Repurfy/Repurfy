ccdee3459b2d6cd33fe2e6d527227c2140083e35
Rahul Kumar <kumar.coding.practice@gmail.com>
2026-01-16 00:14:35 +0530

    Updated Proxy code

--- proxy.ts
+++ proxy.ts
@@ -5,23 +5,15 @@ export default function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname
   const appEnv = process.env.NEXT_PUBLIC_APP_ENV
 
-  // Block only on staging & production
+  console.log('APP ENV FROM MIDDLEWARE 👉', appEnv)
+
   if (appEnv === 'staging' || appEnv === 'production') {
-    // ✅ Allow homepage
     if (pathname === '/') return NextResponse.next()
-
-    // ✅ Allow Next.js internals
     if (pathname.startsWith('/_next')) return NextResponse.next()
+    if (/\.(.*)$/.test(pathname)) return NextResponse.next()
 
-    // ✅ Allow public assets
-    if (/\.(.*)$/.test(pathname)) {
-      return NextResponse.next()
-    }
-
-    // ❌ Redirect everything else
     return NextResponse.redirect(new URL('/', request.url))
   }
 
-  // ✅ dev mode → allow everything
   return NextResponse.next()
 }
