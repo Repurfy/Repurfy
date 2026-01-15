0d0813871ae5ea026c7e3c3bfa310e9c99055b1f
Rahul Kumar <kumar.coding.practice@gmail.com>
2026-01-16 00:00:31 +0530

    Updated URL protection logic

--- proxy.ts
+++ proxy.ts
@@ -3,23 +3,25 @@ import { NextResponse } from 'next/server'
 
 export default function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname
+  const appEnv = process.env.NEXT_PUBLIC_APP_ENV
 
-  // Only block routes in production
-  if (process.env.NODE_ENV === 'production') {
+  // Block only on staging & production
+  if (appEnv === 'staging' || appEnv === 'production') {
     // ✅ Allow homepage
     if (pathname === '/') return NextResponse.next()
 
     // ✅ Allow Next.js internals
     if (pathname.startsWith('/_next')) return NextResponse.next()
 
-    // ✅ Allow public assets (logos, images, fonts, etc.)
+    // ✅ Allow public assets
     if (/\.(.*)$/.test(pathname)) {
       return NextResponse.next()
     }
 
-    // ❌ Redirect everything else to /
+    // ❌ Redirect everything else
     return NextResponse.redirect(new URL('/', request.url))
   }
 
+  // ✅ dev mode → allow everything
   return NextResponse.next()
 }
